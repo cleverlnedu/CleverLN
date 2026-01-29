b0c0dc27741d1280743848a6456d1cc0c3965069
harisha <harsha2002jc@gmail.com>
2026-01-29 23:22:52 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Footer from "@/components/footer/footer";
+import Herohome from "@/components/Hero-homepage/herohome";
 import Hero from "@/components/Hero/Hero";
 import Recommend from "@/components/recomendation/recommend";
+
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Herohome />
       <Recommend />
       <Footer />
     </main>
