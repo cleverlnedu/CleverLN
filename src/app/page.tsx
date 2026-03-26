555d5f2c19872ee5fa39580ddf66efdc736aa286
harisha <harsha2002jc@gmail.com>
2026-03-27 02:18:03 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/footer/footer";
-import Herohome from "@/components/Hero-homepage/herohome";
-import Hero from "@/components/Hero/Hero";
+import Certifie from "@/components/certifie/certifie";
+import Hero from "@/components/Hero/hero_section";
+import Job from "@/components/jobs/jobcard";
 import Recommend from "@/components/recomendation/recommend";
 
 
@@ -8,8 +9,9 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Herohome />
+      <Certifie />
       <Recommend />
+      <Job />
       <Footer />
     </main>
   );
